0396364e391e4774ce29e7a15ebbedeb05bbf365
Replit user <>
2023-03-21 19:33:29 +0000

    feat agregue reproductor youtube, no esta implementado aun, tampoco se renderiza en el componente CardCursos.js

--- src/components/CardCursos.js
+++ src/components/CardCursos.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
 import Paper from '@mui/material/Paper'
 import { Card } from '@mui/material'
+
+function VideoLoop() {
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    // Crea un nuevo reproductor de YouTube
+    const player = new window.YT.Player(playerRef.current, {
+      videoId: 'VFMMbXmaPHk',
+      playerVars: {
+        autoplay: 1,
+        loop: 1,
+        mute: 1,
+        controls: 0,
+        modestbranding: 1,
+      },
+      events: {
+        onReady: (event) => {
+          // Inicia la reproducción del video en loop
+          event.target.playVideo();
+          event.target.setLoop(true);
+        },
+      },
+    });
+
+    return () => {
+      // Destruye el reproductor cuando el componente se desmonta
+      player.destroy();
+    };
+  }, []);
+
+  return <div ref={playerRef}></div>;
+}
 
 const dataUnVideo = {
     titulo: "Mécanica Aeroespacial",
@@ -31,6 +63,7 @@ const mostrarCards = (data) => {
                 >
                     
                 </video>
+              
             </Paper>
         )
     })
